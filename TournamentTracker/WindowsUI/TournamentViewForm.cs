4ed20afac571d0faac26006bc8c19c0d061a7c69
Roddy Ackerman <crossgengames2020@gmail.com>
2020-05-13 19:19:28 -0400

    Iniital Setup of Tournament View

---
+++ TournamentTracker/WindowsUI/TournamentViewForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsUI
+{
+    public partial class TournamentViewForm : Form
+    {
+        public TournamentViewForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TournamentViewForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
