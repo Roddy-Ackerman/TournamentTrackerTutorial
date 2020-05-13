b8f395df498dc143360791eccfafa7f039ebebf6
Roddy Ackerman <crossgengames2020@gmail.com>
2020-05-13 16:00:40 -0400

    Added comments to the library classes

--- TournamentTracker/TrackerLibrary/TeamModel.cs
+++ TournamentTracker/TrackerLibrary/TeamModel.cs
@@ -6,7 +6,13 @@ namespace TrackerLibrary
 {
     public class TeamModel
     {
+        /// <summary>
+        /// List of the team members
+        /// </summary>
         public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        /// <summary>
+        /// The team name
+        /// </summary>
         public string TeamName { get; set; } 
     }
 }
